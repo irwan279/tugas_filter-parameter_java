8ec5807d1a77ea9540f62fa3f0d7a01b10bebdbf
irwan muhamad Faizal <37823549+irwan279@users.noreply.github.com>
2019-11-09 07:14:29 +0700

    tugas filter dan parameter java

---
+++ tugasfilter1.js
@@ -0,0 +1,15 @@
+
+ var arr = [
+   {negara: 'Indonesia', benua: 'Asia'},
+   {negara: 'Jerman', benua: 'Eropa'},
+   {negara: 'Spanyol', benua: 'Eropa'},
+   {negara: 'Korea', benua: 'Asia'},
+   {negara: 'Portugal', benua: 'Eropa'},
+   {negara: 'Amerika Serikat', benua: 'Amerika'},
+ ];
+
+ var benuaEropa = arr.filter(arr => {
+      return arr.benua === "Asia";
+ });
+
+ console.log(benuaEropa);
